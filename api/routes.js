ce54ed1018886927e54c24befde03d4f75c815ae
ayseakyol <ayseakyol.22309@gmail.com>
2020-05-21 04:17:39 +0200

    routes

--- api/routes.js
+++ api/routes.js
@@ -1,11 +1,23 @@
-const controllers = require('./controllers.js');
-const express = require('express');
+const controllers = require("./controllers.js");
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/', controllers.hello);
+router.get("/", controllers.hello);
 
 // write your routes
+router.get("/courses", controllers.getCourses);
 
+// read a file
+router.get("/courses/:id", controllers.readFile);
+
+// write a file
+router.post("/courses", controllers.writeFile);
+
+// put a file
+router.put("/courses/:id", controllers.putFile);
+
+// delete a file
+router.delete("/courses/:id", controllers.deleteFile);
 
 module.exports = router;
